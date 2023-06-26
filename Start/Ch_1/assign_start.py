210b3d5d387289390d59244d1449ebce9540ebc6
Ali Maksudi <39615365+alimaksudi@users.noreply.github.com>
2023-06-26 08:11:40 +0000

    Add the walrus operator to the assign_start.py file.

--- Start/Ch_1/assign_start.py
+++ Start/Ch_1/assign_start.py
@@ -5,14 +5,19 @@ import pprint
 
 
 # regular assignment statements assign a value
-x = 5
-print(x)
+X = 5
+print(X)
 
 # TODO: the assignment operator is part of an expression
-
+# the_str = input("Enter a string: ")
+# while the_str != "quit":
+#     print("You entered: ", the_str)
+#     the_str = input("Enter a string: ")
 
 # TODO: The assignment expression is useful for writing concise code
-
+# Do the same thing with the walrus operator
+while (the_str := input("Enter a string: ")) != "quit":
+    print("You entered: ", the_str)
 
 # TODO: The walrus operator can help reduce redundant function calls
 values = [12, 0, 10, 5, 9, 18, 41, 23, 30, 16, 18, 9, 18, 22]
@@ -21,3 +26,13 @@ val_data = {
     "total": sum(values),
     "average": sum(values) / len(values)
 }
+# pprint.pp(val_data)
+
+# TODO: The walrus operator can help make code more efficient
+val_data2 = {
+    "length": (l := len(values)),
+    "total": (s := sum(values)),
+    "average": s / l
+}
+
+pprint.pp(val_data2)

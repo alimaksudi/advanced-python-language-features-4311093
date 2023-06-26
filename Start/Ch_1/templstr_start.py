16c88da995f9a545a87e2a11c9f247bf6715290d
Ali Maksudi <39615365+alimaksudi@users.noreply.github.com>
2023-06-26 08:34:17 +0000

    Added templstr_start.py to the staging area.

--- Start/Ch_1/templstr_start.py
+++ Start/Ch_1/templstr_start.py
@@ -1,6 +1,6 @@
 # Example file for Advanced Python: Language Features by Joe Marini
 # demonstrate template string functions
-
+from string import Template
 
 # Usual string formatting with f-strings
 str1 = "Advanced Python: Language Features"
@@ -9,7 +9,18 @@ outputstr = f"You're watching {str1} by {str2}"
 print(outputstr)
 
 # TODO: create a template with placeholders
+templ = Template("You're watching ${title} by ${author}")
 
 # TODO: use the substitute method with keyword arguments
+str3 = templ.substitute(
+    title="Advanced Python: Language Features", author="Joe Marini")
+print(str3)
 
 # TODO: use the substitute method with a dictionary
+data = {
+    "author": "Joe Marini",
+    "title": "Advanced Python: Language Features"
+}
+
+str4 = templ.substitute(data)
+print(str4)
